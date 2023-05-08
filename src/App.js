b7db3ef7da70562ee207088d66055bfb4def0322
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-05-08 11:38:11 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <head>
+      <script src="https://kit.fontawesome.com/80f12d827a.js" crossorigin="anonymous"></script>
+    <title>Phantomsong</title>
+      </head>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <h1>Phantomsong</h1>
+        <nav>
+            <ul>
+                <li class="navagation"><a href='index.html'>Home</a></li>
+                <li class="navagation"><a href='sales.html'>Projects</a></li>
+                <li class="navagation"><a href='merch.html'>Merch Shop</a></li>
+                <li class="navagation"><a href='orders.html'>Sign In</a></li>
+            </ul>
+        </nav>
       </header>
     </div>
   );
