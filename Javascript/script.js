b079f1a6a3c8a7c0bce0c770a9f659815c56a9f8
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-10-15 14:33:16 -0500

    mobile test 2

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuButton = document.querySelector('.menu-button');
-  const mobileMenu = document.querySelector('.mobile-menu');
+// JavaScript to toggle between desktop and mobile navigation bars
+function toggleNavbar() {
+  const desktopNavbar = document.getElementById("desktopNavbar");
+  const mobileTopnav = document.getElementById("mobileTopnav");
+  const screenWidth = window.innerWidth;
 
-  menuButton.addEventListener('click', function () {
-    mobileMenu.classList.toggle('active');
-  });
-});
+  if (screenWidth <= 767) {
+    // Display the mobile navigation
+    desktopNavbar.style.display = "none";
+    mobileTopnav.style.display = "block";
+  } else {
+    // Display the desktop navigation
+    desktopNavbar.style.display = "block";
+    mobileTopnav.style.display = "none";
+  }
+}
+
+// Call the toggleNavbar function when the page loads and when the window is resized
+toggleNavbar();
+window.addEventListener("resize", toggleNavbar);
